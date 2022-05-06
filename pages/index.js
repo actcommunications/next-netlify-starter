d9e8cd39c78659f19ab9fe103ff0f3c0b76cde64
ACT Communications <104688454+actcommunications@users.noreply.github.com>
2022-05-07 02:31:32 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,9 +16,8 @@ export default function Home() {
         <p className="description">
           Get started new the best <a href="https://act.net.id" target="_blank">dedicated internet</a> future
         </p>
-        <p>
-        Arti Dedicated
-          Dedicated bandwidth merupakan pelayanan akses internet dari provider yang kecepatan internet tidak dipisah buat pemakai lain. 
+        <p className="description">
+        Arti dedicated bandwidth merupakan pelayanan akses internet dari provider yang kecepatan internet tidak dipisah buat pemakai lain. 
           Soal ini tidak serupa dengan internet broadband di mana kecepatan bandwidth dipisah dengan konsumen setia lain.
           Dedicated internet rata-rata punya rasio kecepatan simetris 1:1 serta jaringan ada di fokus 1 ketimbang dengan internet broadband. 
         </p>
