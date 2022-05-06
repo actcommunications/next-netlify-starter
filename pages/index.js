ed4a609f7e1c19ab794b26f0e42fe8c2265f1fc8
ACT Communications <104688454+actcommunications@users.noreply.github.com>
2022-05-07 02:18:52 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,12 +8,17 @@ export default function Home() {
       <Head>
         <title>Next Dedicated Internet Starter!</title>
         <link rel="icon" href="/favicon.ico" />
+    <meta name="google-site-verification" content="Hburu3vSsj1Y_YhtcGx9VVPNzImURb664X_GqqtFqfU" />
       </Head>
 
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started new the best <a href="https://act.net.id" target="_blank">dedicated internet</a> future
+          Get started new the best <a href="https://act.net.id" target="_blank">dedicated internet</a> future<br><br>
+    Arti Dedicated<br>
+Dedicated bandwidth merupakan pelayanan akses internet dari provider yang kecepatan internet tidak dipisah buat pemakai lain. 
+ Soal ini tidak serupa dengan internet broadband di mana kecepatan bandwidth dipisah dengan konsumen setia lain.<br>
+Dedicated internet rata-rata punya rasio kecepatan simetris 1:1 serta jaringan ada di fokus 1 ketimbang dengan internet broadband. 
         </p>
       </main>
 
